17bc6b7c76ee4a7f506b4cd4c90a0cd886b6528c
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-10-02 23:44:27 +0200

    wip

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,8 +1,10 @@
 <template>
-    <wwLayout class="ww-select-option" path="slot" />
+    <wwLayout class="ww-select-option" path="slot" @click="selectOnClick" />
 </template>
 
 <script>
+import { ref, inject, computed } from 'vue';
+
 export default {
     props: {
         content: { type: Object, required: true },
@@ -10,8 +12,74 @@ export default {
         wwEditorState: { type: Object, required: true },
         /* wwEditor:end */
     },
-    emits: ['triggerClick'],
-    setup(props, { emit }) {},
+    setup(props) {
+        const isEditing = computed(() => {
+            /* wwEditor:start */
+            return props.wwEditorState.isEditing;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        });
+
+        const value = inject('_wwSelectValue');
+        const selectType = inject('_wwSelectType');
+        const setValue = inject('_wwSelectSetValue');
+        const isDisabled = inject('_wwSelectIsDisabled');
+        const isReadonly = inject('_wwSelectIsReadonly');
+
+        const isOptionDisabled = computed(() => props.content.disabled);
+
+        function updateValue(optionValue) {
+            if (selectType.value === 'single') {
+                console.log('single', optionValue);
+                setValue(optionValue);
+            } else if (selectType.value === 'multi') {
+                const currentValue = Array.isArray(value.value) ? value.value : [];
+                const index = currentValue.indexOf(optionValue);
+                if (index === -1) {
+                    setValue([...currentValue, optionValue]);
+                } else {
+                    setValue(currentValue.filter(v => v !== optionValue));
+                }
+                console.log('multi', optionValue);
+            }
+        }
+
+        function selectOnClick() {
+            if (isEditing.value || !props.content.selectOnClick) return;
+            if (!isOptionDisabled.value && !isDisabled.value && !isReadonly.value) {
+                updateValue(props.content.value);
+            }
+        }
+
+        function select() {
+            if (isEditing.value) return;
+            if (!isOptionDisabled.value && !isDisabled.value && !isReadonly.value) {
+                updateValue(props.content.value);
+            }
+        }
+
+        const data = ref({
+            selectValue: value,
+            isOptionDisabled: isOptionDisabled.value,
+        });
+
+        const methods = {
+            select: {
+                description: 'Selects the current option',
+                method: select,
+                editor: {
+                    label: 'Select',
+                    elementName: 'Select Option',
+                    icon: 'cursor-click',
+                },
+            },
+        };
+
+        wwLib.wwElement.useRegisterElementLocalContext('selectOption', data, methods);
+
+        return { selectOnClick };
+    },
 };
 </script>
 
--- ww-config.js
+++ ww-config.js
@@ -16,6 +16,35 @@ export default {
             defaultValue: 'simple',
             section: 'settings',
         },
+        value: {
+            label: { en: 'Value' },
+            type: 'Text',
+            bindable: true,
+        },
+        disabled: {
+            label: { en: 'Disabled' },
+            type: 'OnOff',
+            bindable: true,
+            defaultValue: false,
+            /* wwEditor:start */
+            bindingValidation: {
+                type: 'boolean',
+                tooltip: 'A boolean that defines if the option is disabled: `true | false`',
+            },
+            /* wwEditor:end */
+        },
+        selectOnClick: {
+            label: { en: 'Select on click' },
+            type: 'OnOff',
+            bindable: true,
+            defaultValue: true,
+            /* wwEditor:start */
+            bindingValidation: {
+                type: 'boolean',
+                tooltip: 'A boolean that defines if the option is automatically selected on click: `true | false`',
+            },
+            /* wwEditor:end */
+        },
         slot: {
             hidden: true,
             defaultValue: [],
